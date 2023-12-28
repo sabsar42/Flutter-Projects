9a1840bef0d052de0bda37edb538cf0c3e6688fd
Shakib Absar <sabsar42@gmail.com>
2023-12-29 05:22:28 +0600

    Animate to current postion

--- google_map_project/lib/home_screen.dart
+++ google_map_project/lib/home_screen.dart
@@ -5,26 +5,35 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final Location _locationController = Location();
-  final Completer<GoogleMapController> _mapController =
-  Completer<GoogleMapController>();
+  Completer<GoogleMapController> _controller = Completer();
   static const LatLng _myPos = LatLng(24.893216, 91.855856);
-  static const LatLng _myAppleLoc =
-  LatLng(24.893850690446143, 91.85658598730342);
-  LatLng? _currentP;
 
   @override
   void initState() {
     super.initState();
-    getLocationUpdates();
+    _marker.addAll(_list);
+    _getLocation();
   }
+
+  List<Marker> _marker = [];
+  final List<Marker> _list = const [
+    Marker(
+      markerId: MarkerId('1'),
+      icon: BitmapDescriptor.defaultMarker,
+      position: _myPos,
+      infoWindow: InfoWindow(
+        title: 'My Location',
+        snippet: 'This is my Snippet',
+      ),
+    ),
+  ];
 
   static final CameraPosition _myGooglePlex = const CameraPosition(
     zoom: 15,
@@ -37,83 +46,48 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Stack(
-        children: [
-          GoogleMap(
-            onMapCreated: (GoogleMapController controller) =>
-                _mapController.complete(controller),
-            initialCameraPosition: _myGooglePlex,
-            markers: {
-              Marker(
-                markerId: MarkerId('Current_Location'),
-                icon: BitmapDescriptor.defaultMarker,
-                position: _currentP ?? _myPos,
-              ),
-              Marker(
-                markerId: MarkerId('Source_Loc'),
-                icon: BitmapDescriptor.defaultMarker,
-                position: _myPos,
-              ),
-              Marker(
-                markerId: MarkerId('Destination_Location'),
-                icon: BitmapDescriptor.defaultMarker,
-                position: _myAppleLoc,
-              ),
-            }
-          ),
-          Positioned(
-            bottom: 16,
-            right: 16,
-            child: ElevatedButton(
-              onPressed: () => animateToCurrentLocation(),
-              child: Icon(Icons.my_location),
-            ),
-          ),
-        ],
+      body: SafeArea(
+        child: GoogleMap(
+          zoomControlsEnabled: false,
+          initialCameraPosition: _myGooglePlex,
+          markers: Set.of(_marker),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.my_location_sharp,
+        color: Colors.blueAccent,),
+        backgroundColor: Colors.white,
+        onPressed: () {
+          _getLocation();
+        },
       ),
     );
   }
 
-  Future<void> _cameraToPosition(LatLng pos) async {
-    final GoogleMapController controller = await _mapController.future;
-    await controller.animateCamera(CameraUpdate.newLatLng(pos));
-  }
+  Future<void> _getLocation() async {
+    LocationData locationData;
+    var location = Location();
 
-  Future<void> getLocationUpdates() async {
-    bool _serviceEnabled;
-    PermissionStatus _permissionGranted;
+    try {
+      locationData = await location.getLocation();
+      GoogleMapController controller = await _controller.future;
 
-    _serviceEnabled = await _locationController.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await _locationController.requestService();
-    }
-
-    if (!_serviceEnabled) {
-      return;
-    }
-
-    _permissionGranted = await _locationController.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await _locationController.requestPermission();
-    } else if (_permissionGranted != PermissionStatus.granted) {
-      return;
-    }
-
-    _locationController.onLocationChanged.listen((LocationData currentLocation) {
-      if (currentLocation.latitude != null &&
-          currentLocation.longitude != null) {
-        setState(() {
-          _currentP =
-              LatLng(currentLocation.latitude!, currentLocation.longitude!);
-        });
-        print(_currentP);
-      }
-    });
-  }
-
-  void animateToCurrentLocation() {
-    if (_currentP != null) {
-      _cameraToPosition(_currentP!);
+      controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(
+              locationData.latitude!,
+              locationData.longitude!,
+            ),
+            zoom: 19,
+          ),
+        ),
+      );
+    } catch (e) {
+      print('Error getting location: $e');
     }
   }
 }
