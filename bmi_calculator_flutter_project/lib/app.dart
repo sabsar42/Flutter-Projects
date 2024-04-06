63005f27de0427468e045a39f4c830fef809ad4c
Shakib Absar <sabsar42@gmail.com>
2024-04-06 08:10:51 +0600

    Ongoing

--- bmi_calculator_flutter_project/lib/app.dart
+++ bmi_calculator_flutter_project/lib/app.dart
@@ -1,14 +1,21 @@
 import 'package:bmi_calculator_flutter_project/screen/bmi_screen.dart';
+import 'package:bmi_calculator_flutter_project/switch_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/bindings_interface.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class BmiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BmiScreen(), // Specify your home screen widget here
+      home: BmiScreen(),
+      initialBinding: BindingsBuilder(() {
+        Get.put(SwitchController());
+      }),
     );
   }
 }
--- bmi_calculator_flutter_project/lib/utils/dropdown_utils.dart
+++ bmi_calculator_flutter_project/lib/utils/dropdown_utils.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../widgets/custom_drop_down_menu.dart';
 
@@ -25,13 +26,50 @@ CustomDropdownMenu WeightMetricsDropDownMenu({
   );
 }
 
-CustomDropdownMenu SettingsDropDownMenu({
-  required String dropdownValue,
-  required ValueChanged<String?> onChanged,
-}) {
-  return CustomDropdownMenu(
-    items: ['Settings'],
-    dropdownValue: dropdownValue,
-    onChanged: onChanged,
-  );
+class SettingsCustomDropdownMenu extends StatelessWidget {
+  final List<String> items;
+  final String dropdownValue;
+  final ValueChanged<String?> onChanged;
+  final String labelText;
+
+  const SettingsCustomDropdownMenu({
+    Key? key,
+    required this.items,
+    required this.dropdownValue,
+    required this.onChanged,
+    required this.labelText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField<String>(
+      builder: (FormFieldState<String> state) {
+        return InputDecorator(
+          decoration: InputDecoration(
+            labelText: labelText,
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.deepPurple, width: 10.0),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.deepPurple, width: 10.0),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+          ),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<String>(
+              value: dropdownValue,
+              items: items.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: onChanged,
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
